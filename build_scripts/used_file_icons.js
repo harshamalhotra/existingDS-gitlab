1cb61d2c25131dcf34f235eae2d2912cb764b3fd
Lukas Eipert <leipert@gitlab.com>
2022-12-13 17:40:20 +0100

    Add missing icons for files without type and git submodules

--- build_scripts/used_file_icons.js
+++ build_scripts/used_file_icons.js
@@ -4,9 +4,16 @@ const {
   twoFileExtensionIcons,
 } = require('../src/file_icon_map');
 
-const usedFileIcons = [
-  ...new Set(Object.values({ ...fileNameIcons, ...fileExtensionIcons, ...twoFileExtensionIcons })),
-].sort();
+const uniqueIcons = new Set(
+  Object.values({ ...fileNameIcons, ...fileExtensionIcons, ...twoFileExtensionIcons }),
+);
+
+// These are manually added here used here
+// https://gitlab.com/gitlab-org/gitlab/-/blob/137f333f0313e63ebcb144d788e7be0ab9cfbea2/app/assets/javascripts/vue_shared/components/file_icon.vue#L73
+uniqueIcons.add('file'); // Fallback icon for unknown file types
+uniqueIcons.add('folder-git'); // Used for submodules types
+
+const usedFileIcons = [...uniqueIcons].sort();
 
 // eslint-disable-next-line no-restricted-syntax
 for (const icon of usedFileIcons) {
