khulnasoft-lab/khulnasoft-ui

feat(DuoChat): UI Polish on context
Small styling and layout changes for Duo chat context files
Changelog: changed

src/components/experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_popover/duo_chat_context_item_popover.spec.js
@@ -64,15 +64,19 @@ describe('GlDuoChatContextItemPopover', () => {
 
   describe('item info rendering', () => {
     it.each([
-      ['file', MOCK_CONTEXT_ITEM_FILE],
-      ['issue', MOCK_CONTEXT_ITEM_ISSUE],
-      ['merge request', MOCK_CONTEXT_ITEM_MERGE_REQUEST],
-    ])('renders correct project and type for %s', (_, item) => {
+      ['file', MOCK_CONTEXT_ITEM_FILE, MOCK_CONTEXT_ITEM_FILE.metadata.info.relFilePath],
+      ['issue', MOCK_CONTEXT_ITEM_ISSUE, MOCK_CONTEXT_ITEM_ISSUE.metadata.info.iid.toString()],
+      [
+        'merge request',
+        MOCK_CONTEXT_ITEM_MERGE_REQUEST,
+        MOCK_CONTEXT_ITEM_MERGE_REQUEST.metadata.info.iid.toString(),
+      ],
+    ])('renders correct project and type for %s', (_, item, expected) => {
       createComponent({ item });
 
       const content = findPopover().text();
       expect(content).toContain(item.metadata.info.project);
-      expect(content).toContain(item.type);
+      expect(content).toContain(expected);
     });
 
     it('renders file path for file items', () => {

src/components/experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_popover/duo_chat_context_item_popover.vue
@@ -1,11 +1,13 @@
 <script>
 import GlPopover from '../../../../../../base/popover/popover.vue';
+import GlIcon from '../../../../../../base/icon/icon.vue';
 import { translate } from '../../../../../../../utils/i18n';
 import {
   CONTEXT_ITEM_TYPE_ISSUE,
   CONTEXT_ITEM_TYPE_MERGE_REQUEST,
   CONTEXT_ITEM_TYPE_PROJECT_FILE,
 } from '../constants';
+import GlAlert from '../../../../../../base/alert/alert.vue';
 
 const ID_PREFIXES = {
   issue: '#',
@@ -15,6 +17,8 @@ const ID_PREFIXES = {
 export default {
   name: 'DuoChatContextItemPopover',
   components: {
+    GlAlert,
+    GlIcon,
     GlPopover,
   },
   props: {
@@ -59,6 +63,9 @@ export default {
         ? this.itemInfo.relFilePath || ''
         : null;
     },
+    filePathArray() {
+      return this.filePath?.split('/');
+    },
     isEnabled() {
       return this.item.isEnabled !== false;
     },
@@ -66,6 +73,24 @@ export default {
       return Array.isArray(this.item.disabledReasons) && this.item.disabledReasons.length > 0
         ? this.item.disabledReasons.join(', ')
         : translate('DuoChatContextItemPopover.DisabledReason', 'This item is disabled');
+    },
+    iconName() {
+      if (this.item.type === 'merge_request') {
+        return 'merge-request';
+      }
+      if (this.item.type === 'issue') {
+        return 'issues';
+      }
+      return 'project';
+    },
+    itemTypeLabel() {
+      if (this.item.type === 'merge_request') {
+        return translate('DuoChatContextItemPopover.MergeRequest', 'Merge request');
+      }
+      if (this.item.type === 'issue') {
+        return translate('DuoChatContextItemPopover.Issue', 'Issue');
+      }
+      return translate('DuoChatContextItemPopover.File', 'Project file');
     },
   },
   methods: {
@@ -82,39 +107,43 @@ export default {
     custom-class="gl-duo-chat-item-popover"
   >
     <template #title>
-      <span class="gl-text-sm gl-text-gray-500" data-testid="chat-context-popover-title">{{
-        item.metadata.name
-      }}</span>
+      <div>
+        <div
+          class="gl-heading-3 gl-mb-1 gl-mt-2 gl-leading-1"
+          data-testid="chat-context-popover-title"
+        >
+          {{ item.metadata.name }}
+        </div>
+        <div class="gl-font-normal gl-text-subtle">{{ itemTypeLabel }}</div>
+      </div>
     </template>
-    <div class="gl-p-3">
-      <div class="gl-mb-2">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.ProjectLabel', 'Project:')
-        }}</strong>
-        <span>{{ itemInfo.project }}</span>
+    <div>
+      <div v-if="filePath !== null" class="gl-flex gl-flex-wrap gl-items-center">
+        <gl-icon name="document" :size="12" variant="subtle" class="gl-mr-1" />
+        <span>{{ itemInfo.project }}</span
+        ><span v-for="(pathPart, index) in filePathArray" :key="pathPart"
+          >{{ pathPart }}{{ index + 1 < filePathArray.length ? '/' : '' }}</span
+        >
       </div>
-      <div v-if="filePath !== null" class="gl-mb-2">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.PathLabel', 'Path:')
-        }}</strong>
-        <span>{{ filePath }}</span>
+      <div
+        v-if="filePath === null"
+        class="items-center gl-flex gl-flex-wrap gl-leading-1 gl-text-subtle"
+      >
+        <gl-icon :name="iconName" :size="12" variant="subtle" class="gl-mr-1" />
+
+        <span>{{ itemInfo.project }}</span
+        ><span v-if="id !== null">{{ idPrefix }}{{ id }}</span>
       </div>
-      <div v-if="id !== null" class="gl-mb-2">
-        <strong class="gl-mr-1">{{ translate('DuoChatContextItemPopover.IdLabel', 'ID:') }}</strong>
-        <span>{{ idPrefix }}{{ id }}</span>
-      </div>
-      <div class="gl-mb-2">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.TypeLabel', 'Type:')
-        }}</strong>
-        <span>{{ item.type }}</span>
-      </div>
-      <div v-if="!isEnabled" class="gl-text-red-500" data-testid="chat-context-popover-disabled">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.DisabledMessageLabel', 'Note:')
-        }}</strong>
-        <span>{{ disabledMessage }}</span>
-      </div>
+      <gl-alert
+        v-if="!isEnabled"
+        variant="danger"
+        class="gl-mb-1 gl-mt-3 gl-p-4 gl-text-sm"
+        data-testid="chat-context-popover-disabled"
+        :dismissible="false"
+        :show-icon="false"
+      >
+        {{ disabledMessage }}
+      </gl-alert>
     </div>
   </gl-popover>
 </template>

harshamalhotra/existingDS-gitlab

src/components/experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_popover/duo_chat_context_item_popover.spec.js
@@ -64,15 +64,19 @@ describe('GlDuoChatContextItemPopover', () => {
 
   describe('item info rendering', () => {
     it.each([
-      ['file', MOCK_CONTEXT_ITEM_FILE],
-      ['issue', MOCK_CONTEXT_ITEM_ISSUE],
-      ['merge request', MOCK_CONTEXT_ITEM_MERGE_REQUEST],
-    ])('renders correct project and type for %s', (_, contextItem) => {
+      ['file', MOCK_CONTEXT_ITEM_FILE, MOCK_CONTEXT_ITEM_FILE.metadata.info.relFilePath],
+      ['issue', MOCK_CONTEXT_ITEM_ISSUE, MOCK_CONTEXT_ITEM_ISSUE.metadata.info.iid.toString()],
+      [
+        'merge request',
+        MOCK_CONTEXT_ITEM_MERGE_REQUEST,
+        MOCK_CONTEXT_ITEM_MERGE_REQUEST.metadata.info.iid.toString(),
+      ],
+    ])('renders correct project and type for %s', (_, contextItem, expected) => {
       createComponent({ contextItem });
 
       const content = findPopover().text();
       expect(content).toContain(contextItem.metadata.info.project);
-      expect(content).toContain(contextItem.type);
+      expect(content).toContain(expected);
     });
 
     it('renders file path for file items', () => {

src/components/experimental/duo/chat/components/duo_chat_context/duo_chat_context_item_popover/duo_chat_context_item_popover.vue
@@ -1,5 +1,6 @@
 <script>
 import GlPopover from '../../../../../../base/popover/popover.vue';
+import GlIcon from '../../../../../../base/icon/icon.vue';
 import { translate } from '../../../../../../../utils/i18n';
 import {
   CONTEXT_ITEM_TYPE_ISSUE,
@@ -7,10 +8,13 @@ import {
   CONTEXT_ITEM_TYPE_PROJECT_FILE,
 } from '../constants';
 import { formatIssueId, formatMergeRequestId } from '../utils';
+import GlAlert from '../../../../../../base/alert/alert.vue';
 
 export default {
   name: 'DuoChatContextItemPopover',
   components: {
+    GlAlert,
+    GlIcon,
     GlPopover,
   },
   props: {
@@ -62,6 +66,9 @@ export default {
         ? this.itemInfo.relFilePath || ''
         : null;
     },
+    filePathArray() {
+      return this.filePath?.split('/');
+    },
     isEnabled() {
       return this.contextItem.isEnabled !== false;
     },
@@ -70,6 +77,24 @@ export default {
         this.contextItem.disabledReasons.length > 0
         ? this.contextItem.disabledReasons.join(', ')
         : translate('DuoChatContextItemPopover.DisabledReason', 'This item is disabled');
+    },
+    iconName() {
+      if (this.contextItem.type === 'merge_request') {
+        return 'merge-request';
+      }
+      if (this.contextItem.type === 'issue') {
+        return 'issues';
+      }
+      return 'project';
+    },
+    itemTypeLabel() {
+      if (this.contextItem.type === 'merge_request') {
+        return translate('DuoChatContextItemPopover.MergeRequest', 'Merge request');
+      }
+      if (this.contextItem.type === 'issue') {
+        return translate('DuoChatContextItemPopover.Issue', 'Issue');
+      }
+      return translate('DuoChatContextItemPopover.File', 'Project file');
     },
   },
   methods: {
@@ -86,39 +111,44 @@ export default {
     custom-class="gl-duo-chat-item-popover"
   >
     <template #title>
-      <span class="gl-text-sm gl-text-gray-500" data-testid="chat-context-popover-title">{{
-        contextItem.metadata.name
-      }}</span>
+      <div>
+        <div
+          class="gl-heading-3 gl-mb-1 gl-mt-2 gl-leading-1"
+          data-testid="chat-context-popover-title"
+        >
+          {{ contextItem.metadata.name }}
+        </div>
+        <div class="gl-font-normal gl-text-subtle">{{ itemTypeLabel }}</div>
+      </div>
     </template>
-    <div class="gl-p-3">
-      <div class="gl-mb-2">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.ProjectLabel', 'Project:')
-        }}</strong>
-        <span>{{ itemInfo.project }}</span>
+    <div>
+      <div v-if="filePath !== null" class="gl-flex gl-flex-wrap gl-items-center">
+        <gl-icon name="document" :size="12" variant="subtle" class="gl-mr-1" />
+        <span>{{ itemInfo.project }}</span
+        ><span v-for="(pathPart, index) in filePathArray" :key="pathPart"
+          >{{ pathPart }}{{ index + 1 < filePathArray.length ? '/' : '' }}</span
+        >
       </div>
-      <div v-if="filePath !== null" class="gl-mb-2">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.PathLabel', 'Path:')
-        }}</strong>
-        <span>{{ filePath }}</span>
+
+      <div
+        v-if="filePath === null"
+        class="items-center gl-flex gl-flex-wrap gl-leading-1 gl-text-subtle"
+      >
+        <gl-icon :name="iconName" :size="12" variant="subtle" class="gl-mr-1" />
+
+        <span>{{ itemInfo.project }}</span
+        ><span v-if="id !== null">{{ formattedId }}</span>
       </div>
-      <div v-if="id !== null" class="gl-mb-2">
-        <strong class="gl-mr-1">{{ translate('DuoChatContextItemPopover.IdLabel', 'ID:') }}</strong>
-        <span>{{ formattedId }}</span>
-      </div>
-      <div class="gl-mb-2">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.TypeLabel', 'Type:')
-        }}</strong>
-        <span>{{ contextItem.type }}</span>
-      </div>
-      <div v-if="!isEnabled" class="gl-text-red-500" data-testid="chat-context-popover-disabled">
-        <strong class="gl-mr-1">{{
-          translate('DuoChatContextItemPopover.DisabledMessageLabel', 'Note:')
-        }}</strong>
-        <span>{{ disabledMessage }}</span>
-      </div>
+      <gl-alert
+        v-if="!isEnabled"
+        variant="danger"
+        class="gl-mb-1 gl-mt-3 gl-p-4 gl-text-sm"
+        data-testid="chat-context-popover-disabled"
+        :dismissible="false"
+        :show-icon="false"
+      >
+        {{ disabledMessage }}
+      </gl-alert>
     </div>
   </gl-popover>
 </template>