joolstorrentecalo/gitlab-ui

feat(GlProgressBar): Apply design tokens
* Create contextual design tokens
* Create and import SCSS file to override default with custom
properties
* Create warning and danger stories
* Create a11y specs for warning and danger stories

src/tokens/build/js/tokens.js
@@ -643,6 +643,11 @@ export const GL_LINK_MENTION_TEXT_COLOR_DEFAULT = '#0b5cad'; // Used for the men
 export const GL_LINK_MENTION_TEXT_COLOR_CURRENT = '#703800'; // Used for the mention link text color when referencing the current user.
 export const GL_LINK_MENTION_BACKGROUND_COLOR_DEFAULT = '#cbe2f9'; // Used for the mention link default background.
 export const GL_LINK_MENTION_BACKGROUND_COLOR_CURRENT = '#f5d9a8'; // Used for the mention link background when referencing the current user.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_DEFAULT = '#1f75cb'; // Used for the indicator color for the primary progress-bar type.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_SUCCESS = '#108548'; // Used for the indicator color for the success progress-bar type.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_WARNING = '#ab6100'; // Used for the indicator color for the warning progress-bar type.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_DANGER = '#dd2b0e'; // Used for the indicator color for the danger progress-bar type.
+export const GL_PROGRESS_BAR_TRACK_COLOR = '#bfbfc3'; // Used for the track color for all progress-bar types.
 export const GL_SKELETON_LOADER_BACKGROUND_COLOR = '#dcdcde'; // Used for the skeleton loader background color.
 export const GL_SKELETON_LOADER_SHIMMER_COLOR = '#ececef'; // Used for the animated shimmer effect in a skeleton loader.
 export const GL_SPINNER_TRACK_COLOR_DEFAULT = '#dcdcde'; // Used for the static track (background) of a loading spinner.

harshamalhotra/existingDS-gitlab

src/tokens/build/js/tokens.js
@@ -668,6 +668,11 @@ export const GL_LINK_MENTION_TEXT_COLOR_DEFAULT = '#0b5cad'; // Used for the men
 export const GL_LINK_MENTION_TEXT_COLOR_CURRENT = '#703800'; // Used for the mention link text color when referencing the current user.
 export const GL_LINK_MENTION_BACKGROUND_COLOR_DEFAULT = '#cbe2f9'; // Used for the mention link default background.
 export const GL_LINK_MENTION_BACKGROUND_COLOR_CURRENT = '#f5d9a8'; // Used for the mention link background when referencing the current user.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_DEFAULT = '#1f75cb'; // Used for the indicator color for the primary progress-bar variant.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_SUCCESS = '#108548'; // Used for the indicator color for the success progress-bar variant.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_WARNING = '#ab6100'; // Used for the indicator color for the warning progress-bar variant.
+export const GL_PROGRESS_BAR_INDICATOR_COLOR_DANGER = '#dd2b0e'; // Used for the indicator color for the danger progress-bar variant.
+export const GL_PROGRESS_BAR_TRACK_COLOR = '#bfbfc3'; // Used for the track color for all progress-bar variants.
 export const GL_SKELETON_LOADER_BACKGROUND_COLOR = '#dcdcde'; // Used for the skeleton loader background color.
 export const GL_SKELETON_LOADER_SHIMMER_COLOR = '#ececef'; // Used for the animated shimmer effect in a skeleton loader.
 export const GL_SPINNER_TRACK_COLOR_DEFAULT = '#dcdcde'; // Used for the static track (background) of a loading spinner.