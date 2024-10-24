fa16cc9c0f31094edd0f53088e6848f845163832
Peter Hegman <phegman@gitlab.com>
2024-10-24 12:30:39 -0700

    Remove possessive usage of GitLab
    Per our documentation guidelines
    https://docs.gitlab.com/ee/development/documentation/styleguide/word_list.html#gitlab

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
         </div>
       </main>
       <footer
-        class="gl-flex gl-shrink-0 gl-flex-col gl-items-center gl-justify-center gl-gap-2 gl-p-5 md:gl-flex-row"
+        class="gl-flex gl-shrink-0 gl-flex-col gl-items-center gl-justify-center gl-gap-2 gl-p-5 lg:gl-flex-row"
       >
         <a
           class="with-link-styles"
@@ -18,9 +18,9 @@
           :href="pageSourceHref"
           >View Page Source</a
         >
-        <span class="gl-hidden md:gl-block">|</span>
+        <span class="gl-hidden lg:gl-block">|</span>
         <p>
-          This documentation is specific to GitLab's Tailwind configuration.
+          This documentation is specific to the GitLab Tailwind configuration.
         </p>
       </footer>
     </div>
