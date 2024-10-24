5820d7183bfab28b6b4f3d567d24a1334a825dc4
Peter Hegman <phegman@gitlab.com>
2024-10-24 10:19:40 -0700

    Remove Tailwind logo
    So we don't cause confusion with official docs.
    https://tailwindcss.com/brand

--- components/SidebarLogo.vue
+++ components/SidebarLogo.vue
@@ -7,7 +7,6 @@
       class="gl-h-16 gl-flex gl-shrink-0 gl-items-center gl-gap-x-2"
     >
       <GitLabLogo class="gl-w-8" />
-      <TailwindLogo class="gl-w-10" />
     </RouterLink>
     <OpenSearchButton
       v-if="props.showSearchButton"
@@ -17,7 +16,6 @@
 </template>
 
 <script setup>
-import TailwindLogo from "~/assets/svg/tailwind-logo.svg";
 import GitLabLogo from "~/assets/svg/gitlab-logo.svg";
 
 const emit = defineEmits(["openSearch"]);
