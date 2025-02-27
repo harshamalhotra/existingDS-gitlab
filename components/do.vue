b817e46f2cac271cd31a6dbeb44a97fecd83f860
sdejonge <sdejonge@gitlab.com>
2025-02-28 09:29:44 +1100

    chore: update to use GlBadge in do/dont components

--- components/do.vue
+++ components/do.vue
@@ -1,21 +1,18 @@
 <script>
-import { GlIcon } from '../helpers/gitlab_ui';
+import { GlBadge } from '../helpers/gitlab_ui';
 
 export default {
   components: {
-    GlIcon,
+    GlBadge,
   },
 };
 </script>
 
 <template>
   <div class="md">
-    <strong
-      class="app-styles gl-mb-2 gl-flex gl-items-center gl-gap-2 gl-font-bold gl-text-success"
-    >
-      <gl-icon name="check" variant="success" />
-      Do:
-    </strong>
+    <div class="app-styles gl-mb-2">
+      <gl-badge icon="check" variant="success">Do</gl-badge>
+    </div>
     <slot></slot>
   </div>
 </template>
--- components/dont.vue
+++ components/dont.vue
@@ -1,19 +1,18 @@
 <script>
-import { GlIcon } from '../helpers/gitlab_ui';
+import { GlBadge } from '../helpers/gitlab_ui';
 
 export default {
   components: {
-    GlIcon,
+    GlBadge,
   },
 };
 </script>
 
 <template>
   <div class="md">
-    <strong class="app-styles gl-mb-2 gl-flex gl-items-center gl-gap-2 gl-font-bold gl-text-danger">
-      <gl-icon name="close" variant="danger" />
-      Don’t:
-    </strong>
+    <div class="app-styles gl-mb-2">
+      <gl-badge icon="close" variant="danger">Don’t</gl-badge>
+    </div>
     <slot></slot>
   </div>
 </template>
