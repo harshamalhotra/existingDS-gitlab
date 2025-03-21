0ef0519ed35a19200ae0cc0449940219759f2384
sdejonge <sdejonge@gitlab.com>
2025-03-21 11:24:20 +1100

    chore: add @gitlab/ui and tailwind
    Add @gitlab/ui and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const tailwindDefaults = require('@gitlab/ui/tailwind.defaults');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.{vue,js}', './layouts/**/*.vue', './pages/**/*.vue'],
+  presets: [tailwindDefaults],
+};
