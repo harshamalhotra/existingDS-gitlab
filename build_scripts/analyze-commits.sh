eec44c92188f2481567fad869a9cf0bf9140fce6
Mark Florian <mflorian@gitlab.com>
2025-04-22 17:09:03 +0100

    chore: Detect/publish patch versions

--- build_scripts/analyze-commits.sh
+++ build_scripts/analyze-commits.sh
@@ -8,11 +8,14 @@ if git log "$LAST_TAG..HEAD" --grep "BREAKING CHANGE" | grep "BREAKING CHANGE" >
   >&2 echo "Found a commit with breaking changes"
   >&2 git log "$LAST_TAG..HEAD" --grep "BREAKING CHANGE" --oneline
   echo "major"
-elif git diff "$LAST_TAG..HEAD" --quiet -- sprite_icons illustrations build_scripts/file_icons_version; then
-  >&2 echo "No changes"
-else
+elif ! git diff "$LAST_TAG..HEAD" --quiet -- sprite_icons illustrations build_scripts/file_icons_version; then
+  # Non-zero exit code means there were differences/changes.
   >&2 git diff "$LAST_TAG..HEAD" --name-only -- sprite_icons illustrations build_scripts/file_icons_version
   echo "minor"
+elif git log "$LAST_TAG..HEAD" --grep "^fix:" --no-merges --format=%s | grep "^fix:" > /dev/null; then
+  echo "patch"
+else
+  >&2 echo "No changes"
 fi
 
 exit 0
