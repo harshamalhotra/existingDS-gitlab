814d1975a75c339a82e73df330660ffea7a2e605
Lukas Eipert <leipert@gitlab.com>
2025-05-23 15:43:24 +0200

    Multi deploy all the pages
    This creates three additional pages environments per pipeline:
    
    1. One for storybook: `/storybook` on main, `/review-mr-<iid>-storybook`
       for MRs
    2. One for storybook@vue3: `/storybook-vue3` on main,
       `/review-mr-<iid>-storybook-vue3` for MRs
    3. One for tailwind config viewer: `/tailwind` on main,
       `/review-mr-<iid>-tailwind` for MRs
    
    The existing review app for design.gitlab.com is also tied to the the
    new pages job for `storybook`, so that the iframes are shown from there.
    
    Definitely was a one-shot vibe code thing and we did not need multiple
    attempts because CI is super easy.

--- packages/gitlab-ui/bin/build-storybook-static.sh
+++ packages/gitlab-ui/bin/build-storybook-static.sh
@@ -7,19 +7,15 @@ if [ -z "$CI" ]; then
   exit 1
 fi
 
-DEST=public
-if [ "$VUE_VERSION" = "3" ]; then
-  DEST=public/vue3
-fi
-mkdir -p "$DEST"
+mkdir -p "public"
 
 apt-get update
 apt-get install -y brotli gzip
 echo "Building storybook..."
 yarn storybook:build:prod
-mv storybook/* "$DEST"
+mv storybook/* "public"
 # See: https://docs.gitlab.com/ee/user/project/pages/introduction.html#serving-compressed-assets
 echo "Compressing assets..."
 find public -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|json\|css\|svg\|xml\)$' -exec gzip -f -k {} \;
 find public -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|json\|css\|svg\|xml\)$' -exec brotli -f -k {} \;
-ls -alth "$DEST"
+ls -alth "public"
