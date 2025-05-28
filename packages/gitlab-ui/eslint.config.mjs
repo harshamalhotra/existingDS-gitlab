a992c00273792400cb263c4fc6eaecb0e1bf9b0d
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2025-05-28 07:36:45 -0400

    Ignore bin modules

--- packages/gitlab-ui/eslint.config.mjs
+++ packages/gitlab-ui/eslint.config.mjs
@@ -25,6 +25,7 @@ export default [
       '**/.cypress_cache',
       'src/vendor/',
       '.storybook/docs/',
+      'bin/**/*.mjs',
     ],
   },
   ...compat.extends('plugin:@gitlab/default', 'plugin:storybook/csf', 'plugin:@gitlab/jest'),
