d8631ef0a20b8912cb586e29b1d4f4425837a898
Mark Florian <mflorian@gitlab.com>
2025-06-30 13:12:01 +0100

    Restrict scanned files to actual sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
     './contents/**/*.md',
     './layouts/**/*.vue',
     './pages/**/*.vue',
-    './packages/**/*.{vue,js}',
+    // Scan GitLab UI's own assets
+    './packages/gitlab-ui/src/**/*.{vue,js}',
+    // Ignore specs and stories
+    '!./packages/gitlab-ui/src/**/*.{spec,stories}.js',
+    // Ignore all node_modules
+    '!./**/node_modules/**',
   ],
   presets: [tailwindDefaults],
 };
