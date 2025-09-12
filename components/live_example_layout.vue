d4ceddb7b8c243d4a44c72a330a3a80347800f3e
Jeremy Elder <jelder@gitlab.com>
2025-09-12 00:25:15 -0500

    Make live example source collapsible

--- components/live_example_layout.vue
+++ components/live_example_layout.vue
@@ -1,13 +1,62 @@
-<!-- eslint-disable-next-line vue/no-deprecated-functional-template -->
-<template functional>
+<script>
+import uniqueId from 'lodash/uniqueId';
+import { GlButton } from '../helpers/gitlab_ui';
+
+export default {
+  name: 'LiveExampleLayout',
+  components: {
+    GlButton,
+  },
+  data() {
+    return {
+      isSourceVisible: false,
+    };
+  },
+  computed: {
+    sourcePanelId() {
+      return uniqueId('source-code-panel-');
+    },
+  },
+  methods: {
+    toggleSource() {
+      this.isSourceVisible = !this.isSourceVisible;
+      // Keep focus on the button after toggling
+      this.$nextTick(() => {
+        if (this.$refs.toggleButton && this.$refs.toggleButton.$el) {
+          this.$refs.toggleButton.$el.focus();
+        }
+      });
+    },
+  },
+};
+</script>
+
+<template>
   <div class="gl-border gl-mb-5 gl-flex gl-flex-col gl-rounded-base">
     <div class="background-example-pattern gl-grow gl-basis-0 gl-p-4">
       <slot name="preview"></slot>
     </div>
-    <div
-      class="gl-border-t gl-grow gl-basis-0 gl-rounded-b-base gl-bg-subtle gl-p-3 gl-font-monospace"
-    >
-      <slot name="editor"></slot>
+    <div class="gl-border-t gl-rounded-b-base gl-bg-subtle">
+      <div class="gl-px-3 gl-py-2">
+        <gl-button
+          ref="toggleButton"
+          category="tertiary"
+          variant="confirm"
+          size="small"
+          :aria-expanded="isSourceVisible ? 'true' : 'false'"
+          :aria-controls="sourcePanelId"
+          @click="toggleSource"
+        >
+          {{ isSourceVisible ? 'Hide source' : 'Show source' }}
+        </gl-button>
+      </div>
+      <div
+        v-show="isSourceVisible"
+        :id="sourcePanelId"
+        class="gl-grow gl-basis-0 gl-p-3 gl-font-monospace"
+      >
+        <slot name="editor"></slot>
+      </div>
     </div>
   </div>
 </template>
