20afb808af37cd131ac3a357d5f806f4eb552abe
sdejonge <sdejonge@gitlab.com>
2025-09-15 11:35:35 +1000

    Update spec test

--- packages/gitlab-ui/src/components/base/search_box_by_click/search_box_by_click.spec.js
+++ packages/gitlab-ui/src/components/base/search_box_by_click/search_box_by_click.spec.js
@@ -193,7 +193,7 @@ describe('search box by click component', () => {
       });
 
       it('overrides the border style of the input', () => {
-        expect(wrapper.findComponent(GlFormInput).classes()).toContain('!gl-rounded-default');
+        expect(wrapper.findComponent(GlFormInput).classes()).toContain('!gl-rounded-control');
       });
 
       it('does not add *-with-search-button css modifier', () => {
@@ -213,7 +213,7 @@ describe('search box by click component', () => {
       });
 
       it('does not override the border style of the input', () => {
-        expect(wrapper.findComponent(GlFormInput).classes()).not.toContain('!gl-rounded-default');
+        expect(wrapper.findComponent(GlFormInput).classes()).not.toContain('!gl-rounded-control');
       });
 
       it('adds *-with-search-button css modifier', () => {
