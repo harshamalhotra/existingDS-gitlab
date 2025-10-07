d67164c4d7623cf6e30f6768301ab19be1d50690
sdejonge <sdejonge@gitlab.com>
2025-10-07 15:52:40 +1100

    Add Figma scopes to design tokens
    Use array format for font family

--- packages/gitlab-ui/src/tokens/build/js/tokens.js
+++ packages/gitlab-ui/src/tokens/build/js/tokens.js
@@ -243,10 +243,35 @@ export const GL_COLOR_BRAND_GRAY_03 = '#74717a';
 export const GL_COLOR_BRAND_GRAY_04 = '#45424d';
 export const GL_COLOR_BRAND_GRAY_05 = '#2b2838';
 export const GL_COLOR_BRAND_PINK_01G = '#ffb9c9';
-export const GL_FONT_FAMILY_REGULAR =
-  "var(--default-regular-font, 'GitLab Sans'), -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Noto Sans', Ubuntu, Cantarell, 'Helvetica Neue', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'";
-export const GL_FONT_FAMILY_MONOSPACE =
-  "var(--default-mono-font, 'GitLab Mono'), 'JetBrains Mono', 'Menlo', 'DejaVu Sans Mono', 'Liberation Mono', 'Consolas', 'Ubuntu Mono', 'Courier New', 'andale mono', 'lucida console', monospace";
+export const GL_FONT_FAMILY_REGULAR = [
+  "var(--default-regular-font, 'GitLab Sans')",
+  '-apple-system',
+  'BlinkMacSystemFont',
+  "'Segoe UI'",
+  'Roboto',
+  "'Noto Sans'",
+  'Ubuntu',
+  'Cantarell',
+  "'Helvetica Neue'",
+  'sans-serif',
+  "'Apple Color Emoji'",
+  "'Segoe UI Emoji'",
+  "'Segoe UI Symbol'",
+  "'Noto Color Emoji'",
+];
+export const GL_FONT_FAMILY_MONOSPACE = [
+  "var(--default-mono-font, 'GitLab Mono')",
+  "'JetBrains Mono'",
+  'Menlo',
+  "'DejaVu Sans Mono'",
+  "'Liberation Mono'",
+  'Consolas',
+  "'Ubuntu Mono'",
+  "'Courier New'",
+  "'andale mono'",
+  "'lucida console'",
+  'monospace',
+];
 export const GL_FONT_SIZE_XS = '0.625rem';
 export const GL_FONT_SIZE_SM = '0.75rem';
 export const GL_FONT_SIZE_MD = '0.875rem';
