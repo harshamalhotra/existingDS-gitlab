2d6b6f5b55773a051370134de5c6e2756eef9e42
Thomas Hutterer <thutterer@gitlab.com>
2025-11-03 12:48:18 +0100

    Add JSDoc comments to GlAccordion props
    Fixes comment format from /* */ to /** */ for GlAccordion props
    to support Vetur/IDE autocomplete functionality.

--- packages/gitlab-ui/src/components/base/accordion/accordion.vue
+++ packages/gitlab-ui/src/components/base/accordion/accordion.vue
@@ -11,16 +11,16 @@ export default {
     };
   },
   props: {
-    /*
-    When true, will have the effect of closing other accordion items when one accordion item is visible.
+    /**
+     * When true, will have the effect of closing other accordion items when one accordion item is visible.
      */
     autoCollapse: {
       type: Boolean,
       required: false,
       default: false,
     },
-    /*
-    The header tag used in the accordion (h1/h2/h3/h4/h5/h6). This overrides the value provided by GlAccordion. For accessibility this should be set to an appropriate value in the context where the accordion is used.
+    /**
+     * The header tag used in the accordion (h1/h2/h3/h4/h5/h6). This overrides the value provided by GlAccordion. For accessibility this should be set to an appropriate value in the context where the accordion is used.
      */
     headerLevel: {
       type: Number,
