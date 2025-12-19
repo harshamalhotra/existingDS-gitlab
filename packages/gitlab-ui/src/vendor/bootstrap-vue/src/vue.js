2d10bd0e2552cfd7c8015664bb33310264a3e8f2
Illya Klymov <xanf@xanf.me>
2025-12-19 03:10:14 +0200

    Make render function dynamically react on instance version
    * boostrap-vue components use patched render/h

--- packages/gitlab-ui/src/vendor/bootstrap-vue/src/vue.js
+++ packages/gitlab-ui/src/vendor/bootstrap-vue/src/vue.js
@@ -25,11 +25,7 @@ const ALLOWED_FIELDS_IN_DATA = [
   'refInFor'
 ]
 
-let extend = Vue.extend.bind(Vue)
-
 if (isGlobalVue3) {
-  const { extend: originalExtend } = Vue
-  const KNOWN_COMPONENTS = ['router-link', 'transition', 'transition-group']
   const originalVModelDynamicCreated = Vue.vModelDynamic.created
   const originalVModelDynamicBeforeUpdate = Vue.vModelDynamic.beforeUpdate
 
@@ -64,83 +60,97 @@ if (isGlobalVue3) {
       el[assignSymbol] = function() {}
     }
   }
-  extend = function patchedBootstrapVueExtend(definition) {
-    if (typeof definition === 'object' && definition.render && !definition.__alreadyPatched) {
-      const originalRender = definition.render
-      definition.__alreadyPatched = true
-      definition.render = function(h) {
-        const patchedH = function(tag, dataObjOrChildren, rawSlots) {
-          const slots =
-            rawSlots === undefined
-              ? []
-              : [Array.isArray(rawSlots) ? rawSlots.filter(Boolean) : rawSlots]
-
-          const isTag = typeof tag === 'string' && !KNOWN_COMPONENTS.includes(tag)
-          const isSecondArgumentDataObject =
-            dataObjOrChildren &&
-            typeof dataObjOrChildren === 'object' &&
-            !Array.isArray(dataObjOrChildren)
-
-          if (!isSecondArgumentDataObject) {
-            return h(tag, dataObjOrChildren, ...slots)
-          }
-
-          const { attrs, props, ...restData } = dataObjOrChildren
-          const normalizedData = {
-            ...restData,
-            attrs,
-            props: isTag ? {} : props
-          }
-          if (tag === 'router-link' && !normalizedData.slots && !normalizedData.scopedSlots) {
-            // terrible workaround to fix router-link rendering with compat vue-router
-            normalizedData.scopedSlots = { $hasNormal: () => {} }
-          }
-          return h(tag, normalizedData, ...slots)
-        }
-
-        if (definition.functional) {
-          const ctx = arguments[1]
-          const patchedCtx = { ...ctx }
-          patchedCtx.data = {
-            attrs: { ...(ctx.data.attrs || {}) },
-            props: { ...(ctx.data.props || {}) }
-          }
-          Object.keys(ctx.data || {}).forEach(key => {
-            if (ALLOWED_FIELDS_IN_DATA.includes(key)) {
-              patchedCtx.data[key] = ctx.data[key]
-            } else if (key in ctx.props) {
-              patchedCtx.data.props[key] = ctx.data[key]
-            } else if (!key.startsWith('on')) {
-              patchedCtx.data.attrs[key] = ctx.data[key]
-            }
-          })
-
-          const IGNORED_CHILDREN_KEYS = ['_ctx']
-          const children = ctx.children?.default?.() || ctx.children
-
-          if (
-            children &&
-            Object.keys(patchedCtx.children).filter(k => !IGNORED_CHILDREN_KEYS.includes(k))
-              .length === 0
-          ) {
-            delete patchedCtx.children
-          } else {
-            patchedCtx.children = children
-          }
-
-          patchedCtx.data.on = ctx.listeners
-          return originalRender.call(this, patchedH, patchedCtx)
-        }
-
-        return originalRender.call(this, patchedH)
-      }
-    }
-    return originalExtend.call(this, definition)
-  }.bind(Vue)
 }
 
-const nextTick = Vue.nextTick
-
 const isVue3 = instance => Boolean(instance.$)
+
+const KNOWN_COMPONENTS = ['router-link', 'transition', 'transition-group']
+const { extend: originalExtend } = Vue
+
+const extend = function patchedBootstrapVueExtend(definition) {
+  if (typeof definition === 'object' && definition.render && !definition.__alreadyPatched) {
+    const originalRender = definition.render
+    definition.__alreadyPatched = true
+    definition.render = function(h) {
+      // Vue.js 3 compat checks if function needs to be patched by checking length
+      // of arguments, so we can't put it in function args
+      const functionalCtx = arguments[1]
+
+      const currentInstance = this || functionalCtx?.parent
+      // For Vue 2 instances, call original render directly
+      if (currentInstance && !isVue3(currentInstance)) {
+        return originalRender.call(this, ...arguments)
+      }
+
+      const patchedH = function(tag, dataObjOrChildren, rawSlots) {
+        const slots =
+          rawSlots === undefined
+            ? []
+            : [Array.isArray(rawSlots) ? rawSlots.filter(Boolean) : rawSlots]
+
+        const isTag = typeof tag === 'string' && !KNOWN_COMPONENTS.includes(tag)
+        const isSecondArgumentDataObject =
+          dataObjOrChildren &&
+          typeof dataObjOrChildren === 'object' &&
+          !Array.isArray(dataObjOrChildren)
+
+        if (!isSecondArgumentDataObject) {
+          return h(tag, dataObjOrChildren, ...slots)
+        }
+
+        const { attrs, props, ...restData } = dataObjOrChildren
+        const normalizedData = {
+          ...restData,
+          attrs,
+          props: isTag ? {} : props
+        }
+        if (tag === 'router-link' && !normalizedData.slots && !normalizedData.scopedSlots) {
+          // terrible workaround to fix router-link rendering with compat vue-router
+          normalizedData.scopedSlots = { $hasNormal: () => {} }
+        }
+        return h(tag, normalizedData, ...slots)
+      }
+
+      if (definition.functional) {
+        const ctx = functionalCtx
+        const patchedCtx = { ...ctx }
+        patchedCtx.data = {
+          attrs: { ...(ctx.data.attrs || {}) },
+          props: { ...(ctx.data.props || {}) }
+        }
+        Object.keys(ctx.data || {}).forEach(key => {
+          if (ALLOWED_FIELDS_IN_DATA.includes(key)) {
+            patchedCtx.data[key] = ctx.data[key]
+          } else if (key in ctx.props) {
+            patchedCtx.data.props[key] = ctx.data[key]
+          } else if (!key.startsWith('on')) {
+            patchedCtx.data.attrs[key] = ctx.data[key]
+          }
+        })
+
+        const IGNORED_CHILDREN_KEYS = ['_ctx']
+        const children = ctx.children?.default?.() || ctx.children
+
+        if (
+          children &&
+          Object.keys(patchedCtx.children).filter(k => !IGNORED_CHILDREN_KEYS.includes(k))
+            .length === 0
+        ) {
+          delete patchedCtx.children
+        } else {
+          patchedCtx.children = children
+        }
+
+        patchedCtx.data.on = ctx.listeners
+        return originalRender.call(this, patchedH, patchedCtx)
+      }
+
+      return originalRender.call(this, patchedH, functionalCtx)
+    }
+  }
+  return originalExtend.call(this, definition)
+}.bind(Vue)
+
+const nextTick = Vue.nextTick
 
 export { COMPONENT_UID_KEY, Vue, mergeData, isGlobalVue3, isVue3, nextTick, extend }
