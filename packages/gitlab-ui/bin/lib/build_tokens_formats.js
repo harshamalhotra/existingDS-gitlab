7096bb8f2ba49c84fd8d065742fbbf15f5d28bef
Scott de Jonge <sdejonge@gitlab.com>
2026-02-10 14:56:09 +1100

    Remove fontWeight condition

--- packages/gitlab-ui/bin/lib/build_tokens_formats.js
+++ packages/gitlab-ui/bin/lib/build_tokens_formats.js
@@ -557,7 +557,7 @@ function resolveFigmaValue(token) {
 
   // Ensure numeric values are proper JavaScript numbers
   // Converts string representations of numbers to actual number types
-  if (type === 'number' || type === 'fontWeight') {
+  if (type === 'number') {
     return Number(value);
   }
 
