79f006c654a67c9ceb450393da1871a4f368dd96
Mark Florian <mflorian@gitlab.com>
2026-03-02 12:48:36 +0000

    Check for untracked files in built tokens
    See
    https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/-/merge_requests/5684
    for context.

--- packages/gitlab-ui/bin/check_tokens_build.sh
+++ packages/gitlab-ui/bin/check_tokens_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 yarn build-tokens
-git diff --exit-code ./src/tokens/build
-if [ $? -ne 0 ]; then
-  echo "Tokens build is outdated. Please run \`yarn build-tokens\` and commit the changes"
+if [ -n "$(git status --porcelain=v2 ./src/tokens/build)" ]; then
+  git status ./src/tokens/build
+  echo "Tokens build is outdated. See above for the details. To fix this, run \`yarn build-tokens\` and commit the changes."
   exit 1
 else
   echo "Tokens build is up-to-date."
