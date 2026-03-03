18b757e07eedd34935c893cdbbf39d21050e5f95
Scott de Jonge <sdejonge@gitlab.com>
2026-03-03 18:47:25 +1100

    Update live-example to use GlCard
    Update live-example to use GlCard to visually align with Storybook
    iframe embeds using GlCard

--- components/live_example_layout.vue
+++ components/live_example_layout.vue
@@ -1,11 +1,12 @@
 <script>
 import uniqueId from 'lodash/uniqueId';
-import { GlButton } from '../helpers/gitlab_ui';
+import { GlButton, GlCard } from '../helpers/gitlab_ui';
 
 export default {
   name: 'LiveExampleLayout',
   components: {
     GlButton,
+    GlCard,
   },
   data() {
     return {
@@ -32,24 +33,28 @@ export default {
 </script>
 
 <template>
-  <div class="gl-border gl-mb-5 gl-flex gl-flex-col gl-rounded-default">
-    <div class="background-example-pattern gl-grow gl-basis-0 gl-p-4 gl-@container">
-      <slot name="preview"></slot>
-    </div>
-    <div class="gl-border-t gl-rounded-b-default gl-bg-subtle">
-      <div class="gl-px-3 gl-py-2">
-        <gl-button
-          ref="toggleButton"
-          category="tertiary"
-          variant="confirm"
-          size="small"
-          :aria-expanded="isSourceVisible ? 'true' : 'false'"
-          :aria-controls="sourcePanelId"
-          @click="toggleSource"
-        >
-          {{ isSourceVisible ? 'Hide source' : 'Show source' }}
-        </gl-button>
+  <gl-card
+    class="gl-mb-5"
+    body-class="background-example-pattern gl-@container"
+    footer-class="!gl-p-0 !gl-pt-2"
+  >
+    <template #default>
+      <div class="gl-relative gl-z-2">
+        <slot name="preview"></slot>
       </div>
+    </template>
+    <template #footer>
+      <gl-button
+        ref="toggleButton"
+        category="tertiary"
+        variant="confirm"
+        size="small"
+        :aria-expanded="isSourceVisible ? 'true' : 'false'"
+        :aria-controls="sourcePanelId"
+        @click="toggleSource"
+      >
+        {{ isSourceVisible ? 'Hide source' : 'Show source' }}
+      </gl-button>
       <div
         v-show="isSourceVisible"
         :id="sourcePanelId"
@@ -57,6 +62,6 @@ export default {
       >
         <slot name="editor"></slot>
       </div>
-    </div>
-  </div>
+    </template>
+  </gl-card>
 </template>
